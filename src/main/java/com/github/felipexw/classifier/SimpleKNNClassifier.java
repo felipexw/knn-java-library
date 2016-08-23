4067c92713f3ecc32c4ca108cdd36e1b8f96a7e5
felipexw <felipexw@gmail.com>
2016-08-23 18:22:41 -0300

    Add k and features inside knn algorithm.

--- src/main/java/com/github/felipexw/classifier/SimpleKNNClassifier.java
+++ src/main/java/com/github/felipexw/classifier/SimpleKNNClassifier.java
@@ -4,6 +4,9 @@ package com.github.felipexw.classifier;
  * Created by felipe.appio on 23/08/2016.
  */
 public class SimpleKNNClassifier extends Classifier {
+    private short k;
+    private long features[][];
+
     @Override
     void train() {
 
