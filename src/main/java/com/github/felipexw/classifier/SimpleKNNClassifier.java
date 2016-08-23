21931a284ea17b7dc7b34d83bb875fed879ce9e9
felipexw <felipexw@gmail.com>
2016-08-23 14:55:47 -0300

    Add SimpleKNNClassifier.

---
+++ src/main/java/com/github/felipexw/classifier/SimpleKNNClassifier.java
@@ -0,0 +1,16 @@
+package com.github.felipexw.classifier;
+
+/**
+ * Created by felipe.appio on 23/08/2016.
+ */
+public class SimpleKNNClassifier extends Classifier {
+    @Override
+    void train() {
+
+    }
+
+    @Override
+    double predict() {
+        return 0;
+    }
+}
