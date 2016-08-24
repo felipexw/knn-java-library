79a5e85e627a45bc0cd12007bca8dc9639b3b26a
felipexw <felipexw@gmail.com>
2016-08-24 18:02:32 -0300

    Add predict method.

--- src/main/java/com/github/felipexw/neighbors/SimpleKNNClassifier.java
+++ src/main/java/com/github/felipexw/neighbors/SimpleKNNClassifier.java
@@ -2,8 +2,11 @@ package com.github.felipexw.neighbors;
 
 import com.github.felipexw.classifier.Classifier;
 import com.github.felipexw.metrics.DistanceCalculator;
+import com.github.felipexw.types.Instance;
+import com.github.felipexw.types.LabeledInstance;
 import com.github.felipexw.types.LabeledTrainingInstance;
 import com.github.felipexw.types.PredictedInstance;
+import com.google.common.collect.Maps;
 
 import java.util.*;
 
@@ -13,7 +16,7 @@ import java.util.*;
 public class SimpleKNNClassifier extends Classifier {
     private int k;
     private double[][] features;
-    private List<LabeledTrainingInstance>  instances;
+    private List<LabeledTrainingInstance> instances;
     private DistanceCalculator distanceCalculator;
 
     public SimpleKNNClassifier(DistanceCalculator distanceCalculator) {
@@ -26,7 +29,7 @@ public class SimpleKNNClassifier extends Classifier {
         if (instances == null || instances.isEmpty())
             throw new IllegalArgumentException("Instances for training can't be null or empty.");
 
-        this.instances  = instances;
+        this.instances = instances;
     }
 
     public void setK(int k) {
@@ -43,27 +46,52 @@ public class SimpleKNNClassifier extends Classifier {
             throw new IllegalArgumentException("Instance to predict can't be null");
 
         List<Neighboor> neighboorses = new ArrayList<>();
-        for(short i=0; i  < instances.size(); i++){
+        for (short i = 0; i < instances.size(); i++) {
             LabeledTrainingInstance trainingInstance = instances.get(i);
-            double distance  = distanceCalculator.calculate(labeledInstance.getFeatures(), trainingInstance.getFeatures());
+            double distance = distanceCalculator.calculate(labeledInstance.getFeatures(), trainingInstance.getFeatures());
 
             Neighboor neighboor = new Neighboor(trainingInstance, distance);
             neighboorses.add(neighboor);
         }
 
         neighboorses = getKNearestNeighbors(neighboorses);
-
-        return new PredictedInstance("", 0d);
+        return getLabelByMajorityVote(neighboorses);
     }
 
-    private String getLabelByMajorityVote(double[] knnVotes){
-        int max = Integer.MIN_VALUE;
+    private PredictedInstance getLabelByMajorityVote(List<Neighboor> neighboors) {
+        Map<String, Integer> votes = new HashMap<>();
 
-        return null;
+        for (Neighboor neighboor : neighboors) {
+            LabeledInstance instance = neighboor.getInstance();
+            if (!votes.containsKey(instance.getLabel()))
+                votes.put(instance.getLabel(), 1);
+
+            else{
+                Integer count = votes.get(instance.getLabel());
+                votes.put(instance.getLabel(), count+1);
+            }
+        }
+        return getMajorLabel(votes);
     }
 
-    public List<Neighboor> getKNearestNeighbors(List<Neighboor> neighboors){
-        Collections.sort(neighboors, (  nei1,   nei2) -> {
+    private PredictedInstance getMajorLabel(Map<String, Integer> votes){
+        Integer min = Integer.MAX_VALUE;
+        Set<String> labels = votes.keySet();
+        String votedLabel = null;
+
+        for(String label: labels){
+            Integer vote = votes.get(label);
+            if (min > vote){
+                min= vote;
+                votedLabel = label;
+            }
+
+        }
+        return new PredictedInstance(votedLabel, 1/min);
+    }
+
+    public List<Neighboor> getKNearestNeighbors(List<Neighboor> neighboors) {
+        Collections.sort(neighboors, (nei1, nei2) -> {
             if (nei2.getDistance() > nei1.getDistance())
                 return -1;
             return 0;
