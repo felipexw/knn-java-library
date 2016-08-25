f96a047e9581894abde84089057157a118f15038
felipexw <felipexw@gmail.com>
2016-08-25 09:52:34 -0300

    Add ManhattanDistanceCalculator

---
+++ src/main/java/com/github/felipexw/metrics/ManhattanDistanceCalculator.java
@@ -0,0 +1,19 @@
+package com.github.felipexw.metrics;
+
+/**
+ * Created by felipe.appio on 25/08/2016.
+ */
+public class ManhattanDistanceCalculator implements DistanceCalculator {
+    @Override
+    public double calculate(double[] a, double[] b) {
+        if (a == null || a.length == 0)
+            throw new IllegalArgumentException("The params can't be null or empty.");
+        double distance = 0;
+
+        for (short i = 0; i < a.length; i++) {
+            double difference = b[i] - a[i];
+            distance += difference;
+        }
+        return distance;
+    }
+}
