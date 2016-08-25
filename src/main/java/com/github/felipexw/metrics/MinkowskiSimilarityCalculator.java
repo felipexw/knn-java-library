471344ac1922df3dc1129e1dd70a9a7a4d44d5db
felipexw <felipexw@gmail.com>
2016-08-25 13:22:35 -0300

    Add MinkowskiSimilarityCalculator

---
+++ src/main/java/com/github/felipexw/metrics/MinkowskiSimilarityCalculator.java
@@ -0,0 +1,12 @@
+package com.github.felipexw.metrics;
+
+/**
+ * Created by felipe.appio on 25/08/2016.
+ */
+public class MinkowskiSimilarityCalculator implements SimilarityCalculator {
+
+    @Override
+    public double calculate(double[] a, double[] b) {
+        return 0;
+    }
+}
