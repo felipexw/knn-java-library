30b839c0938893c098dd1c08b1885e9f4617bd44
felipexw <felipexw@gmail.com>
2016-08-25 13:56:30 -0300

    MahalanobisSimilarityCalculator setups

--- src/main/java/com/github/felipexw/metrics/MahalanobisSimilarityCalculator.java
+++ src/main/java/com/github/felipexw/metrics/MahalanobisSimilarityCalculator.java
@@ -3,5 +3,9 @@ package com.github.felipexw.metrics;
 /**
  * Created by felipe.appio on 25/08/2016.
  */
-public class MahalanobisSimilarityCalculator {
+public class MahalanobisSimilarityCalculator implements  SimilarityCalculator{
+    @Override
+    public double calculate(double[] a, double[] b) {
+        return 0;
+    }
 }
