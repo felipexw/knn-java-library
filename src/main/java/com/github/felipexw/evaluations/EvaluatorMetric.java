da1765ba6f577ea92702428d915e9f874ca6167e
felipexw <felipexw@gmail.com>
2016-08-26 10:22:41 -0300

    Add EvaluatorMetric to calcuate fMeasure, accuracy, recall and precision

---
+++ src/main/java/com/github/felipexw/evaluations/EvaluatorMetric.java
@@ -0,0 +1,26 @@
+package com.github.felipexw.evaluations;
+
+/**
+ * Created by felipe.appio on 26/08/2016.
+ */
+public class EvaluatorMetric {
+
+    public double accuracy(double[] a, double[] b){
+        if ((a == null || a.length ==1 ) ||  (b == null || b.length == 1))
+            throw new IllegalArgumentException("The args can't be invalid.");
+
+        return 0d;
+    }
+
+    public double fMeasure(){
+        return 0d;
+    }
+
+    public double precision(){
+        return 0d;
+    }
+
+    public double recall(){
+        return 0d;
+    }
+}
