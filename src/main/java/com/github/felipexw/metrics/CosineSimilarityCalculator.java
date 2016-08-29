882b56c61a9af0bd2e3e8ba7b2b8a17c360b9c13
felipexw <felipexw@gmail.com>
2016-08-29 09:56:24 -0300

    Add cosine similarity and tests case.

--- src/main/java/com/github/felipexw/metrics/CosineSimilarityCalculator.java
+++ src/main/java/com/github/felipexw/metrics/CosineSimilarityCalculator.java
@@ -6,6 +6,21 @@ package com.github.felipexw.metrics;
 public class CosineSimilarityCalculator implements SimilarityCalculator{
     @Override
     public double calculate(double[] a, double[] b) {
-        return 0;
+        if (a == null || b == null || (a.length != b.length))
+            throw new IllegalArgumentException("args can't be invalid");
+
+        double numerator = 0d;
+        double denominator = 0d;
+        double aFeatures = 0d;
+        double bFeatures = 0d;
+
+        for(int i =0; i < a.length; i++){
+            numerator += a[i] * b[i];
+            denominator += (Math.pow(a[i], 2)) * (Math.pow(b[i], 2));
+        }
+
+//        denominator = (Math.sqrt(aFeatures)) * (Math.sqrt(bFeatures));
+
+        return numerator/denominator;
     }
 }
--- src/test/java/SimilarityCalculatorCalculatorTest.java
+++ src/test/java/SimilarityCalculatorCalculatorTest.java
@@ -16,7 +16,7 @@ public class SimilarityCalculatorCalculatorTest {
     }
 
     @Test
-    public void it_should_calculate_the_euclidian_distance_between_5_And_2(){
+    public void it_should_calculate_the_euclidian_distance_between_5_And_2() {
         SimilarityCalculator similarityCalculator = new EuclidianSimilarityCalculator();
         double found = similarityCalculator.calculate(new double[]{3}, new double[]{5});
         double expected = Math.round(2.449489742783178);
@@ -24,32 +24,68 @@ public class SimilarityCalculatorCalculatorTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void when_calculate_the_pearson_correlation_with_empty_point_it_should_raise_an_exception(){
+    public void when_calculate_the_pearson_correlation_with_empty_point_it_should_raise_an_exception() {
         SimilarityCalculator similarityCalculator = new PearsonSimilarityCalculator();
         similarityCalculator.calculate(null, null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void when_calculate_the_manhattan_distance_with_empty_point_it_should_raise_an_exception(){
+    public void when_calculate_the_manhattan_distance_with_empty_point_it_should_raise_an_exception() {
         SimilarityCalculator similarityCalculator = new ManhattanSimilarityCalculator();
         similarityCalculator.calculate(null, null);
     }
 
     @Test
-    public void it_should_calculate_the_mahattan_distance_between_5_And_2(){
+    public void it_should_calculate_the_mahattan_distance_between_5_And_2() {
         SimilarityCalculator similarityCalculator = new ManhattanSimilarityCalculator();
-        double found = similarityCalculator.calculate(new double[]{3,2, 50}, new double[]{5,2, 10});
+        double found = similarityCalculator.calculate(new double[]{3, 2, 50}, new double[]{5, 2, 10});
         double expected = 42d;
 
         Truth.assertThat(found).isEqualTo(expected);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void when_calculate_the_jaccard_distance_with_invalid_args_it_should_raise_an_exception(){
+    public void when_calculate_the_jaccard_distance_with_invalid_args_it_should_raise_an_exception() {
         SimilarityCalculator similarityCalculator = new JaccardSimilarityCalculator();
         similarityCalculator.calculate(null, null);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void when_calculate_the_cosine_distance_with_invalid_args_it_should_raise_an_exception() {
+        SimilarityCalculator similarityCalculator = new CosineSimilarityCalculator();
+        similarityCalculator.calculate(null, null);
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void when_calculate_the_cosine_distance_with_different_args_dimensions_it_should_raise_an_exception() {
+        SimilarityCalculator similarityCalculator = new CosineSimilarityCalculator();
+        similarityCalculator.calculate(new double[3], new double[2]);
+    }
+
+    @Test()
+    public void when_calculate_the_jaccard_distance_between_two_feature_vectors() {
+        /**
+         * A(2,2); B(2,2)
+         */
+        SimilarityCalculator similarityCalculator = new JaccardSimilarityCalculator();
+        double similarity = similarityCalculator.calculate(new double[]{2d, 2d}, new double[]{2d, 2d});
+        double expectedSimilarity = 0d;
+
+        Truth.assertThat(similarity)
+                .isEqualTo(expectedSimilarity);
+    }
+
+    @Test()
+    public void when_calculate_the_cosine_distance_between_two_feature_vectors() {
+        /**
+         * A(2,2); B(2,2)
+         */
+        SimilarityCalculator similarityCalculator = new JaccardSimilarityCalculator();
+        double similarity = similarityCalculator.calculate(new double[]{2d, 2d}, new double[]{2d, 2d});
+        double expectedSimilarity = 0d;
+
+        Truth.assertThat(similarity)
+                .isEqualTo(expectedSimilarity);
+    }
 
 }
